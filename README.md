49534fd52a8880ac1f372a7fd1aca94eaf6a6af0
utkarsh <us190190@gmail.com>
2024-04-04 01:14:59 +0530

    config moved to env

--- README.md
+++ README.md
@@ -1,16 +1,42 @@
-Messenger Service
+# Messenger [Service]
 
+## Features
+1. User management - Register, Update, Authenticate, Remove
+2. Messaging supported:
+   1. personal messages between any two users
+   2. group messages among members of the group
+3. If a user is connected, then they receive personal or group messages immediately
+4. Offline users get both type of messages, when they come online
+5. Messages are end-to-end encrypted using SSL/TLS encryption
+
+## In-progress
+1. Frontend application to consume these backend messenger service
+2. Refactoring the repo
+3. Adding database migrations and seeder for the backend service
+
+## Architecture diagram
+1.
+
+## Low level design
+1.
+
+## Flow diagram
+1.
+
+## Steps to deploy backend service on GCP
+1.
+
+## Steps to deploy docker container on local
+1.
+
+## Steps to utilize postman collection
+1.
+
+## Known issues
+1. Both live messages don't contain sender user information (sender is required to share his/her info along with each message)
+2. Need to manually add VM instance public IP to allow connectivity with DB instance
 
 TODO:
-1. P0: Deliver group message to offline users at next connection
-2. 
-3. Setup document in READme
-4. Add postman collection
-5. architecture diagram
-6. flow diagram
-7. 
-8. # GCP infra launch config
-   1. config.yaml in DockerFile
-   2. commit files in git repo, add steps in READme
-
-P.S. Allow VM IP to network settings manually
+# GCP infra launch config
+1. config.yaml in DockerFile
+2. commit files in git repo, add steps in READme
--- main.go
+++ main.go
@@ -5,30 +5,14 @@ import (
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/us190190/messenger/database"
 	"github.com/us190190/messenger/services"
-	"gopkg.in/yaml.v2"
-	"io/ioutil"
 	"log"
 	"net/http"
-	"os"
 )
 
-type Config struct {
-	Database struct {
-		Host     string `yaml:"host"`
-		Port     string `yaml:"port"`
-		User     string `yaml:"user"`
-		Password string `yaml:"password"`
-	} `yaml:"database"`
-}
-
 func main() {
-	err, done := InitConfig()
-	if !done {
-		return
-	}
 
 	// Initialize database connection
-	err = database.InitDB()
+	err := database.InitDB()
 	if err != nil {
 		log.Fatal("Application unable to connect with DB")
 		return
@@ -50,42 +34,5 @@ func main() {
 		Handler:   nil, // Default router
 		TLSConfig: tlsConfig,
 	}
-	log.Fatal(server.ListenAndServeTLS("/app/server.crt", "/app/server.key"))
-}
-
-func InitConfig() (error, bool) {
-	// Read config.yaml file
-	configFile, err := ioutil.ReadFile("config.yaml")
-	if err != nil {
-		log.Fatalf("Failed to read config file: %v", err)
-		return err, false
-	}
-	// Parse YAML
-	var config Config
-	if err = yaml.Unmarshal(configFile, &config); err != nil {
-		log.Fatalf("Failed to unmarshal config: %v", err)
-		return err, false
-	}
-	// Set environment variables
-	err = os.Setenv("DB_HOST", config.Database.Host)
-	if err != nil {
-		log.Fatalf("Failed to set env: %v", err)
-		return err, false
-	}
-	err = os.Setenv("DB_PORT", config.Database.Port)
-	if err != nil {
-		log.Fatalf("Failed to set env: %v", err)
-		return err, false
-	}
-	err = os.Setenv("DB_USER", config.Database.User)
-	if err != nil {
-		log.Fatalf("Failed to set env: %v", err)
-		return err, false
-	}
-	err = os.Setenv("DB_PASSWORD", config.Database.Password)
-	if err != nil {
-		log.Fatalf("Failed to set env: %v", err)
-		return err, false
-	}
-	return nil, true
+	log.Fatal(server.ListenAndServeTLS("server.crt", "server.key"))
 }
